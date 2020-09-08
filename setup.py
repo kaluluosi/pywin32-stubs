a9cbc509c008ecb4289122858d88865ed1d9397e
dengxuan <303359166@qq.com>
2020-09-08 19:09:28 +0800

    更新版本，新增win32helper

--- setup.py
+++ setup.py
@@ -16,22 +16,24 @@ def create_pth():
     lines.append(os.path.join(pkg_dir, "win32-stubs\n"))
     lines.append(os.path.join(pkg_dir, "pythonwin-stubs\n"))
     lines.append(os.path.join(pkg_dir, "win32comext-stubs\n"))
+    lines.append(os.path.join(pkg_dir, "win32helper\n"))
     with open(os.path.join(site_package_path,'pywin32-stubs.pth'), 'w', encoding='utf-8') as f:
         f.writelines(lines)
 
 setup(
     name="pywin32-stubs",
-    version="0.1.0",
+    version="0.1.2",
     description="stubs for pywin32",
     license="MIT",
     author="Carlos Teng",
     author_email ="303359166@qq.com",
     keywords=["pywin32", "stubs"],
-    packages=["win32-stubs", "pythonwin-stubs", "win32comext-stubs"],
+    packages=["win32-stubs", "pythonwin-stubs", "win32comext-stubs", "win32helper"],
     package_data={
         "win32-stubs":["*.py", "*.pyi"],
         "pythonwin-stubs":["*.py", "*.pyi"],
-        "win32comext-stubs":["*.py", "*.pyi"]
+        "win32comext-stubs":["*.py", "*.pyi"],
+        "win32helper":["*.py", "*.pyi"]
         },
     install_requires=['pywin32'],
     long_description=long_description,
